6bb939dc6d2a2c645dc4adc47afc5c3c2bb2eff9
kopelevi <adi.kopelevich@hp.com>
2015-10-16 14:04:41 +0300

    added JMX remote JVM args

--- runner/src/main/bin/start.bat
+++ runner/src/main/bin/start.bat
@@ -1,16 +1,19 @@
 @echo off
 
-rem Uncomment and set if  not set by the calling environment
+rem Should be set in case  not set by env
 rem JAVA_HOME=
 
 rem JVM arguments - remote debug argument
 set "JAVA_OPTS=%JAVA_OPTS% -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
 
-rem # JVM arguments - GC options
-rem set "JAVA_OPTS=%JAVA_OPTS% -XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
+rem JVM arguments - GC options
+set "JAVA_OPTS=%JAVA_OPTS% -XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
 
-rem # JVM arguments - print startup JVM argument
+rem JVM arguments - enable jmx remote access
+set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
+
+rem JVM arguments - print startup JVM argument
 set "JAVA_OPTS=%JAVA_OPTS% -XX:+PrintCommandLineFlags"
 
-rem # Start Server
+rem Start Server
 "%JAVA_HOME%/bin/java" %JAVA_OPTS% -jar ../webapp/WEB-INF/lib/store-runner-1.0-SNAPSHOT.jar
--- runner/src/main/bin/start.sh
+++ runner/src/main/bin/start.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-# Uncomment and set in not set by the calling environment
+# Should be set in case  not set by env
 #JAVA_HOME=
 
 # JVM arguments - remote debug argument
 #JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
 
 # JVM arguments - GC options
-#JAVA_OPTS="$JAVA_OPTS -XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
 
 # JVM arguments - print startup JVM argument
 JAVA_OPTS="$JAVA_OPTS -XX:+PrintCommandLineFlags"
+
+# JVM arguments - enable jmx remote access
+JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 
 # Start Server
 "$JAVA_HOME/bin/java" $JAVA_OPTS -jar ../webapp/WEB-INF/lib/store-runner-1.0-SNAPSHOT.jar
