482a862c526d9c2d0b7d0cf637cda4cfff7716b0
kopelevi <adi.kopelevich@hp.com>
2015-10-28 16:48:31 +0200

    introduced ItemServiceException

---
+++ service/src/main/java/sample/task/list/service/ItemServiceException.java
@@ -0,0 +1,11 @@
+package sample.task.list.service;
+
+/**
+ * Created by kopelevi on 28/10/2015.
+ */
+public class ItemServiceException extends RuntimeException {
+
+    public ItemServiceException(String msg) {
+        super(msg);
+    }
+}
--- service/src/main/java/sample/task/list/service/ItemServiceItemNotFoundException.java
+++ service/src/main/java/sample/task/list/service/ItemServiceItemNotFoundException.java
@@ -3,7 +3,7 @@ package sample.task.list.service;
 /**
  * Created by kopelevi on 19/10/2015.
  */
-public class ItemServiceItemNotFoundException extends RuntimeException {
+public class ItemServiceItemNotFoundException extends ItemServiceException {
 
     public ItemServiceItemNotFoundException(int itemId) {
         super("Item with ID: " + itemId + ", was not found.");
