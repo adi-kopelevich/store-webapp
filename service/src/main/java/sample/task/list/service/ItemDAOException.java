9d720f57493c2ef191e6e52c38ab847970ef4c14
kopelevi <adi.kopelevich@hp.com>
2015-10-28 18:14:32 +0200

    remove logs from DAO layer to enable top level logging on REST service level

--- service/src/main/java/sample/task/list/service/ItemDAOException.java
+++ service/src/main/java/sample/task/list/service/ItemDAOException.java
@@ -1,22 +1,15 @@
 package sample.task.list.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Created by kopelevi on 28/10/2015.
  */
 public class ItemDAOException extends RuntimeException {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ItemDAOException.class);
-
     public ItemDAOException(String msg) {
         super(msg);
-        LOGGER.error(msg);
     }
 
-    public ItemDAOException(String msg,  Exception e) {
+    public ItemDAOException(String msg, Exception e) {
         super(msg, e);
-        LOGGER.error(msg, e);
     }
 }
