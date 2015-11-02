316781b1d71ead792a94e917c5fc65089c88439c
kopelevi <adi.kopelevich@hp.com>
2015-11-02 21:53:14 +0200

    add perf test to allow monitoring of memory and check concurency

--- runner/src/test/java/sample/task/list/service/ItemServicePerfIT.java
+++ runner/src/test/java/sample/task/list/service/ItemServicePerfIT.java
@@ -1,5 +1,15 @@
 package sample.task.list.service;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by kopelevi on 16/10/2015.
  */
@@ -8,12 +18,40 @@ public class ItemServicePerfIT {
     private static final int PORT = 8080;
     private static final String HOST = "localhost";
 
-//    @Test
+    @Test
     public void whenAddItemThenItIsRetrievable() {
         ItemsService itemsService = new ItemServiceClientImpl(HOST, PORT);
-        for (int i = 0; i < 1000000; i++) {
-            itemsService.getAllItems();
-            System.out.println(i);
+        itemsService.clearAll();
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (int i = 0; i < 10000; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + ": started...");
+                    int itemId = new Random().nextInt();
+                    String itemName = UUID.randomUUID().toString();
+                    String itemCategory = UUID.randomUUID().toString();
+                    long itemReminder = new Random().nextLong();
+                    List<String> itemNotes = Arrays.asList(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+
+                    TaskItem item = new TaskItem(itemId, itemName, itemCategory, itemReminder, itemNotes);
+
+                    itemsService.addItem(item);
+
+                    int numOfItems = itemsService.getAllItems().size();
+                    System.out.println(Thread.currentThread().getName() + ": numOfItems=" + numOfItems);
+                }
+            });
+
+        }
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            itemsService.clearAll();
         }
     }
 
