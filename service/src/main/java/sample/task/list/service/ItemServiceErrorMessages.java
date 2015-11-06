527d11960f8165be892fab132f5737b26b699bcb
kopelevi <adi.kopelevich@hp.com>
2015-11-06 10:54:49 +0200

    additional error messages

--- service/src/main/java/sample/task/list/service/ItemServiceErrorMessages.java
+++ service/src/main/java/sample/task/list/service/ItemServiceErrorMessages.java
@@ -11,4 +11,8 @@ public class ItemServiceErrorMessages {
     public static final String FAILED_TO_REMOVE_ITEM = "Failed to remove item with id: ";
     public static final String FAILED_TO_UPDATE_ITEM = "Failed to update item: ";
     public static final String FAILED_TO_CLEAR_ALL = "Failed to clear all items";
+    public static final String INVALID_PARAM_NON_POSITIVE_ID = "Task item ID should be positive, given value is ";
+    public static final String ITEM_WITH_GIVEN_ID_WAS_NOT_FOUND = "Item with given ID was not found: ";
+
+
 }
