38c8a2849265e552e99a0520d37770464aeb3eb8
kopelevi <adi.kopelevich@hp.com>
2015-11-07 13:43:01 +0200

    fix error messages coming from DAO

--- service/src/main/java/sample/task/list/service/ItemServiceErrorMessages.java
+++ service/src/main/java/sample/task/list/service/ItemServiceErrorMessages.java
@@ -5,12 +5,12 @@ package sample.task.list.service;
  */
 public class ItemServiceErrorMessages {
 
-    public static final String FAILED_TO_GET_ALL = "Failed to get all items";
-    public static final String FAILED_TO_GET_ITEM = "Failed to get item with id: ";
-    public static final String FAILED_TO_ADD_ITEM = "Failed to add item: ";
-    public static final String FAILED_TO_REMOVE_ITEM = "Failed to remove item with id: ";
-    public static final String FAILED_TO_UPDATE_ITEM = "Failed to update item: ";
-    public static final String FAILED_TO_CLEAR_ALL = "Failed to clear all items";
+    public static final String FAILED_TO_GET_ALL = "Failed to invoke get all items";
+    public static final String FAILED_TO_GET_ITEM = "Failed to invoke get item with id: ";
+    public static final String FAILED_TO_ADD_ITEM = "Failed to invoke add item: ";
+    public static final String FAILED_TO_REMOVE_ITEM = "Failed to invoke remove item with id: ";
+    public static final String FAILED_TO_UPDATE_ITEM = "Failed to invoke update item: ";
+    public static final String FAILED_TO_CLEAR_ALL = "Failed to invoke clear all items";
     public static final String INVALID_PARAM_NON_POSITIVE_ID = "Task item ID should be positive, given value is ";
     public static final String ITEM_WITH_GIVEN_ID_WAS_NOT_FOUND = "Item with given ID was not found: ";
 
